e96ef0526f4253e3d93d52fd7d9fafd8aab20838
muzrea <muzrea@hotmail.com>
2017-06-24 16:29:37 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -1,18 +1,16 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-	o) 最大值 = ${sequence.maximum()}
-	o) 元素数量 = ${sequence.number()}
-	o) 平均值 = ${sequence.average()}
-    `);
+    console.log(
+    	'o) 最小值 = ${sequence.minimum()}
+		o) 最大值 = ${sequence.maximum()}
+		o) 序列中的元素个数 = ${sequence.number()}
+		o) 平均值 = ${sequence.average()}');
+	// Write your code here
 };
 
 class Sequence {
   constructor(input) {
-  	 var min,
-	max,
-	sum = 0;
+  	 var min,max,sum = 0;
     for(var i=0;i<input.length;i++){
 	var value = input[i];
         sum += value;
